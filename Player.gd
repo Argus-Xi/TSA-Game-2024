6b49620ea5b772de97de92d2d1d75196d0d029ce
haveneg <97711931+haveneg@users.noreply.github.com>
2024-01-18 07:39:39 -0500

    Player added

---
+++ Player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+export var speed = 100
+var omega = PI
+var direction = Vector2.ZERO
+var velocity = Vector2.ZERO
+
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		velocity.x += -1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+
+	if Input.is_action_pressed("up"):
+		velocity.y += -1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if velocity != Vector2.ZERO:
+		direction = velocity
+	
+	rotation = direction.angle()
+		
+	position += velocity.normalized() * speed * delta
